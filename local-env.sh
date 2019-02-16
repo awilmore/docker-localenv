de3f40c5e788fec1351b78d60014e72488cd095d
Adam Wilmore <awilmore@assemblypayments.com>
2019-02-16 12:24:26 +1100

    Support for host-mounted .aws config and custom ports

--- local-env.sh
+++ local-env.sh
@@ -4,12 +4,20 @@ set -e
 
 LOCALENV="awilmore/localenv"
 
-if [ $# != 1 ]; then
-  echo "usage: $0 container_name"
+if [ $# -lt 1 ]; then
+  echo "usage: $0 container_name (ext_port int_port)";
   exit 1
 fi
 
 NAME=$1
+EXT=$2
+INT=$3
+
+PORTS="-p 8000 -p 8080 -p 9000"
+
+if [ ! -z "$EXT" ]; then
+  PORTS="$PORTS -p $EXT:$INT"
+fi
 
 # Convert local path to container path
 CPATH=$( echo $PWD | sed "s#${HOME}#/root#" )
@@ -21,14 +29,13 @@ docker run -it \
   --privileged \
   --name $NAME \
   --hostname $NAME \
-  -p 8000 \
-  -p 8080 \
-  -p 9000 \
+  $PORTS \
   -v ${HOME}/git:/root/git \
   -v ${HOME}/work:/root/work \
   -v ${HOME}/sbin:/root/sbin \
   -v ${HOME}/.ssh_local_env:/root/.ssh \
   -v ${HOME}/go:/root/go \
+  -v ${HOME}/.aws:/root/.aws \
   -v /tmp:/tmp \
   $LOCALENV \
   bash -c "cd ${CPATH} && /root/startup.sh"
